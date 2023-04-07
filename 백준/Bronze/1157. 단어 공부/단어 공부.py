4b6b3d19b286b8915896f8ff232e582ff3e7f4c9
yuuhey <118980404+yuuhey@users.noreply.github.com>
2023-04-07 22:25:04 +0900

    [Bronze I] Title: 단어 공부, Time: 204 ms, Memory: 120032 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,8 @@
+import collections
+c = collections.Counter(input().upper())
+lst = list(c.values())
+max_value = max(lst)
+if lst.count(max_value)==1: # 개수가 하나이면
+    print(list(c.keys())[lst.index(max_value)])
+else:
+    print("?")
