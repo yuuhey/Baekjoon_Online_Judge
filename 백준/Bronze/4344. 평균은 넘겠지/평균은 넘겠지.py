f7797570051cadbbdec415cfce12ae64167197f1
yuuhey <118980404+yuuhey@users.noreply.github.com>
2023-04-20 04:12:00 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 124 ms, Memory: 114984 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,13 +1,11 @@
-num = int(input())
-
-for _ in range(num):
-    scores = list(map(int, input().split()))
-    avg = sum(scores[1:])/scores[0]
-    
-    cnt = 0
-    for i in scores[1:]:
-        if i > avg:
-            cnt += 1
-            
-    per = (cnt/scores[0])*100
-    print('%.3f' %per + '%')
+N = int(input())
+for _ in range(N):
+    lst = list(map(int, input().split()))
+    avg = sum(lst[1:])/lst[0]
+    count = 0
+    for i in lst[1:]:
+        if i>avg:
+            count += 1
+    # print(str('{:.3f}'.format(round(count/lst[0]*100, 3)))+"%")
+    per = (count/lst[0])*100
+    print('%.3f' % per + '%')
